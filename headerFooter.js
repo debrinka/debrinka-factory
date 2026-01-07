097aac4331f77e9363985bfc90551715ce79d2a6
debrinka <erideba@gmail.com>
2026-01-07 19:32:23 +0100

    change link hover menù

--- headerFooter.js
+++ headerFooter.js
@@ -117,10 +117,30 @@ $(document).ready(function () {
         <div id="side-menu" style="display:none; flex-direction: column; justify-content: center; align-items: center;">
             <button id="close-menu">close</button>
             <ul>
-                <li><a href="index.html">home</a></li>
-                <li><a href="works.html">works</a></li>
-                <li><a href="fanzine.html">fanzine</a></li>
-                <li><a href="mailto:erideba@gmail.com">contact</a></li>
+                <li><a href="index.html" class="menu-link">
+                    <span class="menu-icon">
+                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.292893 11.6064C-0.0976311 11.9969 -0.0976311 12.6301 0.292893 13.0206C0.683418 13.4111 1.31658 13.4111 1.70711 13.0206L0.292893 11.6064ZM13.3137 0.999768C13.3137 0.447483 12.866 -0.000232004 12.3137 -0.000232257L3.31371 -0.000231456C2.76142 -0.000231793 2.31371 0.447484 2.31371 0.999768C2.31371 1.55205 2.76142 1.99977 3.31371 1.99977L11.3137 1.99977L11.3137 9.99977C11.3137 10.5521 11.7614 10.9998 12.3137 10.9998C12.866 10.9998 13.3137 10.5521 13.3137 9.99977L13.3137 0.999768ZM1.70711 13.0206L13.0208 1.70688L11.6066 0.292661L0.292893 11.6064L1.70711 13.0206Z"/>
+                        </svg>
+                    </span>
+                    <span class="menu-text">home</span></a></li>
+                <li><a href="try_works.html" class="menu-link">
+                    <span class="menu-icon">
+                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.292893 11.6064C-0.0976311 11.9969 -0.0976311 12.6301 0.292893 13.0206C0.683418 13.4111 1.31658 13.4111 1.70711 13.0206L0.292893 11.6064ZM13.3137 0.999768C13.3137 0.447483 12.866 -0.000232004 12.3137 -0.000232257L3.31371 -0.000231456C2.76142 -0.000231793 2.31371 0.447484 2.31371 0.999768C2.31371 1.55205 2.76142 1.99977 3.31371 1.99977L11.3137 1.99977L11.3137 9.99977C11.3137 10.5521 11.7614 10.9998 12.3137 10.9998C12.866 10.9998 13.3137 10.5521 13.3137 9.99977L13.3137 0.999768ZM1.70711 13.0206L13.0208 1.70688L11.6066 0.292661L0.292893 11.6064L1.70711 13.0206Z"/>
+                        </svg>
+                    </span>
+                    <span class="menu-text">works</span></a></li>
+                <li><a href="mailto:erideba@gmail.com" class="menu-link">
+                    <span class="menu-icon">
+                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.292893 11.6064C-0.0976311 11.9969 -0.0976311 12.6301 0.292893 13.0206C0.683418 13.4111 1.31658 13.4111 1.70711 13.0206L0.292893 11.6064ZM13.3137 0.999768C13.3137 0.447483 12.866 -0.000232004 12.3137 -0.000232257L3.31371 -0.000231456C2.76142 -0.000231793 2.31371 0.447484 2.31371 0.999768C2.31371 1.55205 2.76142 1.99977 3.31371 1.99977L11.3137 1.99977L11.3137 9.99977C11.3137 10.5521 11.7614 10.9998 12.3137 10.9998C12.866 10.9998 13.3137 10.5521 13.3137 9.99977L13.3137 0.999768ZM1.70711 13.0206L13.0208 1.70688L11.6066 0.292661L0.292893 11.6064L1.70711 13.0206Z"/>
+                        </svg>
+                    </span>
+                    <span class="menu-text">contact</span></a></li>
             </ul>
         </div>`;
         $('body').append(menuHTML);
@@ -152,5 +172,16 @@ $(document).on('click', '#menu-trigger', function (e) {
         e.preventDefault();
         $('#side-menu').hide('slide', { direction: 'up' }, 500);
     });
+
+// LINK ATTIV NEL MENU
+    const currentPage = window.location.pathname.split('/').pop();
+
+$('#side-menu a').each(function () {
+    const linkPage = $(this).attr('href');
+
+    if (linkPage === currentPage) {
+        $(this).addClass('is-active');
+    }
+});
 
 });
