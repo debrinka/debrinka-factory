abb0a193f01b6044eaf6c1c05f08fca5463b6738
debrinka <erideba@gmail.com>
2026-01-19 22:56:15 +0100

    scrollHorizontal without animation

--- horizontalScroll.js
+++ horizontalScroll.js
@@ -7,9 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
 function setSectionHeight() {
   const trackWidth = track.scrollWidth;
   const viewportWidth = window.innerWidth;
- const endPadding = viewportWidth * 0.4;
-  section.style.height =
-    trackWidth - viewportWidth + window.innerHeight + endPadding +'px';
+  
+  section.style.height = trackWidth - viewportWidth + window.innerHeight + 'px';
 }
 
   function onScroll() {
@@ -19,59 +18,16 @@ function setSectionHeight() {
 
   // ritardo prima di partire
   const startOffset = 150;
-    const maxScroll = track.scrollWidth - window.innerWidth + window.innerWidth * 0.4;
+    const maxScroll = track.scrollWidth - window.innerWidth;
   // progress parte solo dopo il delay
   const progress = Math.min(
-    Math.max(scrollInside - startOffset, 0),
+    Math.max(scrollInside, 0),
     maxScroll
   );
     track.style.transform = `translateX(-${progress}px)`;
-    updateJourneyBackground(progress);
-     updateActiveYearCard();
      setSectionHeight();
   }
 
-function updateJourneyBackground(progress) {
-  const maxScroll = track.scrollWidth - window.innerWidth;
-  const ratio = progress / maxScroll;
-
-  if (ratio < 0.2) {
-    document.body.style.backgroundColor = 'var(--white)';
-  } else {
-    document.body.style.backgroundColor = 'var(--black)';
-  }
-}
-
-function updateActiveYearCard() {
-  const cards = document.querySelectorAll('.year-card');
-  const viewportWidth = window.innerWidth;
-
-  const focusX = viewportWidth * 0.35; // punto orizzontale della card attiva, 35% dello schermo
-  const range = viewportWidth * 0.6; // range di influenza delle trasformazioni, entro 
-
-  cards.forEach((card, index) => {
-
-    // PRIMA CARD: fissa
-    if (index === 0) {
-      card.style.transform = 'translateY(0%) scale(1)';
-      return;
-    }
-
-    const rect = card.getBoundingClientRect(); //posizione della card nello schermo
-    const distance = rect.left - focusX; // distanza orizzontale dal punto di focus (0 è esattamente sul punto, negativo a sinistra, positivo a destra)
-
-    let t = 1 - Math.min(Math.max(distance / range, 0), 1);
-    t = t * t * (3 - 2 * t); // smoothstep
-
-    const scale = 0.85 + t * 0.15;
-    const translateY = (1 - t) * 25;
-
-    card.style.transform = `
-      translateY(${translateY}%)
-      scale(${scale})
-    `;
-  });
-}
 
  /* --------------------
      ENABLE / DISABLE
